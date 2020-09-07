0e14ac8acdca7e77c0d4a023553c346cae60ceb9
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-09-07 09:11:39 +0530

    Create Struct1.cpp

---
+++ Struct/Struct1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct point
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    struct point p;
+    p.x = 10;
+    p.y = 20;
+    cout << p.x << " " << p.y;
+    return 0;
+}
